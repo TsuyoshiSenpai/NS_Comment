0d441338fa34a4561583403effea421d3946574b
TsuyoshiSenpai <valike.ershoff@yandex.ru>
2022-03-16 19:57:30 +0300

    upd16.03

---
+++ View/StartWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NS_Comment.View
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewMode(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void EditMode(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReadMode(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
